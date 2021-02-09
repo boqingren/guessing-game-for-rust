7311c6c6b4007a53e2fb28a22564c5ba9a67032f
Jun <boqingren@hotmail.com>
2021-02-09 15:59:47 +0800

    fourth version of guessing game

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,23 @@ fn main() {
 
     println!("秘密号码是: {}", secret_number);
 
-    println!("请输入你猜的数字，以回车键结束：");
+    loop {
+        println!("请输入你答案，以回车键结束：");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("无法从命令行获取有效的输入！");
+        io::stdin().read_line(&mut guess).expect("无法从命令行获取有效的输入！");
 
-    let guess: u32 = guess.trim().parse().expect("请输入合法的数字！");
+        let guess: u32 = guess.trim().parse().expect("请输入合法的数字！");
 
-    println!("你的猜测的数字: {}", guess);
+        println!("你输入的数字是: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("结果：太小了！"),
-        Ordering::Greater => println!("结果：太大了"),
-        Ordering::Equal => println!("结果：恭喜你猜对了，正确的答案是：{}", secret_number)
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("结果：太小了！"),
+            Ordering::Greater => println!("结果：太大了"),
+            Ordering::Equal => println!("结果：恭喜你猜对了，正确的答案是：{}", secret_number)
+        }
+
+        println!("====================================");
     }
 }
