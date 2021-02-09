fec3d740ec7fa2a8de1b715c917e0845ab6a1883
Jun <boqingren@hotmail.com>
2021-02-09 06:38:01 +0000

    first version of guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("=== 猜数字游戏 ===");
+
+    println!("请输入你猜的数字，以回车键结束：");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("无法从命令行获取有效的输入！");
+
+    println!("你的猜测的数字: {}", guess);
 }
